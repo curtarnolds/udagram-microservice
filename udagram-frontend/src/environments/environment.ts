445a5eee962647b027856bbb5606e762d8005597
curtarnold <afayedormawuli@gmail.com>
2022-07-16 02:28:39 +0000

    container orchestration with kubernetes and kubectl

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a78abc08a1b2041f485e105735974b44-463480454.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
